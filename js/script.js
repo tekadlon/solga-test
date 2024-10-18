89e7c17412c2c51e4fa7f089889963edfc905ad5
tekadlon <pomkelomke@gmail.com>
2024-10-18 14:08:51 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ var loadingGif = document.getElementById('loading-gif');
 var sendingText = document.getElementById('sending-text');
 var initialText = document.getElementById('initialText');
 
-// надо не забыть найти подходящее регулярное выражение
+// Валидация email
 emailInput.addEventListener('input', function() {
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (emailPattern.test(emailInput.value)) {
